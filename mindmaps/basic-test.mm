f2f2fe9998eb05376d5a6fa539f937811af567bf
Peter Yates <pyates@gmail.com>
2013-12-15 11:59:28 -0600

    Added initial test mindmap and raw xml version

---
+++ mindmaps/basic-test.mm
@@ -0,0 +1,169 @@
+<map version="freeplane 1.2.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Root" ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1387129340081" STYLE="bubble"><hook NAME="MapStyle">
+    <properties show_note_icons="true"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node">
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right">
+<stylenode LOCALIZED_TEXT="default" MAX_WIDTH="600" COLOR="#000000" STYLE="as_parent">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.note"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<node TEXT="Section 1" POSITION="right" ID="ID_1937520074" CREATED="1387129340614" MODIFIED="1387129350693">
+<node TEXT="Topic 1" ID="ID_607425089" CREATED="1387129351829" MODIFIED="1387129599626"><richcontent TYPE="NOTE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      This is a sentence.
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      <u>Underlined sentence.</u>
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      <i>Italics!</i>
+    </p>
+    <p>
+      <b>This is bold!</b>
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      * Some
+    </p>
+    <p>
+      * Bullet
+    </p>
+    <p>
+      * Points
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      1. an
+    </p>
+    <p>
+      1. ordered
+    </p>
+    <p>
+      1. list
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node TEXT="Topic 2" ID="ID_658584224" CREATED="1387129361269" MODIFIED="1387129363031"/>
+<node TEXT="Topic 3" ID="ID_184446365" CREATED="1387129363315" MODIFIED="1387129364719"/>
+</node>
+<node TEXT="Section 2" POSITION="left" ID="ID_1501499118" CREATED="1387129340614" MODIFIED="1387129608167">
+<node TEXT="Topic 1" ID="ID_81771131" CREATED="1387129351829" MODIFIED="1387129599626"><richcontent TYPE="NOTE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      This is a sentence.
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      <u>Underlined sentence.</u>
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      <i>Italics!</i>
+    </p>
+    <p>
+      <b>This is bold!</b>
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      * Some
+    </p>
+    <p>
+      * Bullet
+    </p>
+    <p>
+      * Points
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      1. an
+    </p>
+    <p>
+      1. ordered
+    </p>
+    <p>
+      1. list
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node TEXT="Topic 2" ID="ID_1501450355" CREATED="1387129361269" MODIFIED="1387129363031"/>
+<node TEXT="Topic 3" ID="ID_47195846" CREATED="1387129363315" MODIFIED="1387129364719"/>
+</node>
+</node>
+</map>
